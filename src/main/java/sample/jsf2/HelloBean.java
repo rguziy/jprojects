97903f1a43f3c883d35114c93b8570054c3e18f4
rguziy <rguziy@gmail.com>
2012-10-03 10:14:49 +0300

    Move annotations to bean class from faces-config file.

--- src/main/java/sample/jsf2/HelloBean.java
+++ src/main/java/sample/jsf2/HelloBean.java
@@ -2,6 +2,11 @@ package sample.jsf2;
 
 import java.io.Serializable;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
 public class HelloBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
